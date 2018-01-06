e186d0b9e4291ad360e8ad0a991a0ec8ea2c9b5a
Bjoern Ebbrecht <ebbrecht@expalas.de>
2018-01-06 01:53:58 +0000

    first travis test

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use bjoernffm\e6b\Calculator as e6bCalc;
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$result = e6bCalc::getWindCorrectionAngle(220, 110, 270, 25);
-var_dump($result);
